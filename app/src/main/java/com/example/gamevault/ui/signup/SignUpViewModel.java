507ac58d2e32a217646f6ea613976b68b55cbbf0
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-01-22 18:31:55 -0500

    Login Page Completed

---
+++ app/src/main/java/com/example/gamevault/ui/signup/SignUpViewModel.java
@@ -0,0 +1,7 @@
+package com.example.gamevault.ui.signup;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignUpViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/gamevault/ui/signup/signUpFragment.java
@@ -0,0 +1,34 @@
+package com.example.gamevault.ui.signup;
+
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.gamevault.R;
+
+public class signUpFragment extends Fragment {
+
+    private SignUpViewModel mViewModel;
+
+    public static signUpFragment newInstance() {
+        return new signUpFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
