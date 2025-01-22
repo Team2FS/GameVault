615c19204e028a526b1822b680fc812ad0de7800
Andy Kilgore <kilgoreandy@ymail.com>
2025-01-21 21:53:05 -0600

    Added OKhttp3 for discord webhook class to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
