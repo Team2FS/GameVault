bf6f9f1a8d8885517374a7bcec1e27dbfac0e31a
Andy Kilgore <kilgoreandy@icloud.com>
2025-02-04 22:46:21 -0600

    -- got a good example of a recycler view working without api

--- app/src/main/java/com/example/gamevault/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/gamevault/ui/home/HomeFragment.java
@@ -8,7 +8,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.gamevault.R;
 import com.example.gamevault.databinding.FragmentHomeBinding;
+import com.example.gamevault.ui.home.newsfeed.CustomAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -20,8 +27,20 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         View root = binding.getRoot();
-        return root;
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_news);
+        recyclerView.setAdapter(new CustomAdapter(generateData()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
+    }
+    private List<String> generateData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data.add(String.valueOf(i) + "th Element");
+        }
+        return data;
     }
 
     @Override
