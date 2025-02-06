33d6d017b82de7b4ad6b0009c34ff0a6ff538509
Andy Kilgore <kilgoreandy@icloud.com>
2025-02-05 23:58:11 -0600

    -- hooked up the free api to the recycler view and now pull in news feed for bo6. - just need to enable the links somehow.

--- app/src/main/java/com/example/gamevault/ui/home/newsfeed/CustomAdapter.java
+++ app/src/main/java/com/example/gamevault/ui/home/newsfeed/CustomAdapter.java
@@ -3,28 +3,38 @@ package com.example.gamevault.ui.home.newsfeed;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.gamevault.ui.home.HomeFragment.Article;
 import com.example.gamevault.R;
 
 import java.util.List;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
-    private List<String> data;
-    public CustomAdapter (List<String> data){
+    private List<Article> data;
+    public CustomAdapter (List<Article> data){
         this.data = data;
     }
 
     @Override
     public CustomAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_view, parent, false);
+        View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_newscard, parent, false);
         return new ViewHolder(rowItem);
     }
 
     @Override
     public void onBindViewHolder(CustomAdapter.ViewHolder holder, int position) {
-        holder.textView.setText(this.data.get(position));
+        Article article = data.get(position);
+        holder.textViewTitle.setText(article.getTitle());
+        holder.textViewDescription.setText(article.getDescription());
+
+        // Load image using Glide
+        Glide.with(holder.imageView.getContext())
+                .load(article.getImageUrl())
+                .into(holder.imageView);
     }
 
     @Override
@@ -33,17 +43,21 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private TextView textView;
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private ImageView imageView;
 
         public ViewHolder(View view) {
             super(view);
             view.setOnClickListener(this);
-            this.textView = view.findViewById(R.id.textview);
+            this.textViewTitle = view.findViewById(R.id.article_title);
+            this.textViewDescription = view.findViewById(R.id.article_description);
+            this.imageView = view.findViewById(R.id.article_image);
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(), "position : " + getLayoutPosition() + " text : " + this.textView.getText(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(view.getContext(), "tapped on link", Toast.LENGTH_SHORT).show();
         }
     }
 }
