58ab644c8799d66cc835e82a9f01772e36a7ca8c
Thomas Parson <TJParson@student.fullsail.edu>
2025-02-10 11:07:08 -0500

    SocialFragment further changes

--- app/src/main/java/com/example/gamevault/ui/social/socialFragment.java
+++ app/src/main/java/com/example/gamevault/ui/social/socialFragment.java
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.List;
+import com.example.gamevault.R;
 
 
 public class socialFragment extends Fragment {
@@ -22,6 +23,37 @@ public class socialFragment extends Fragment {
     private RecyclerView chatRecyclerView;
     private EditText messageInput;
     private Button sendButton;
-    private
+    private  ChatAdapter chatAdapter;
+    private List<Message> messageList;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_social, container, false);
+        chatRecyclerView = view.findViewById(R.id.chatRecyclerView);
+        messageInput = view.findViewById(R.id.messageInput);
+        sendButton = view.findViewById(R.id.sendButton);
+
+        messageList = new ArrayList<>();
+        chatAdapter = new ChatAdapter(messageList);
+        chatRecyclerView.setAdapter(chatAdapter);
+        chatRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+       // loadProfileImage << ur gonna forget to do this DONT FORGET TO DO THIS
+         loadMessages();
+
+        sendButton.setOnClickListener(v -> sendMessage());
+        return view;
+    }
+
+    private void loadMessages() {
+        //idk what im doing with this lol
+    }
+
+    private void sendMessage() {
+        String message = messageInput.getText().toString();
+
+
+    }
 
 }
