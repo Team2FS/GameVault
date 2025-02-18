d3f8ea9155adf554781861bd19922f23244b43d2
Andy Kilgore <92450298+kilgoreandy@users.noreply.github.com>
2025-02-18 00:32:23 -0600

    Andy_Kilgore_DEV (#55)
    * Setting up dummy version of recycler view for tracking challenges part 1
    
    Updated header of home to welcome text and changed welcome text to tracked fields
    
    * Final changes before pushing to DEV. Adjusted margins for cleaner look.
    
    Changed to use id instead of hardcoding strings

--- app/src/main/java/com/example/gamevault/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/gamevault/ui/home/HomeFragment.java
@@ -18,6 +18,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.gamevault.R;
 import com.example.gamevault.databinding.FragmentHomeBinding;
+import com.example.gamevault.ui.home.favoritechallenges.CustomAdapterFavorites;
 import com.example.gamevault.ui.home.newsfeed.CustomAdapter;
 import com.example.gamevault.ui.settings.manageprofile.ManageProfileActivity;
 import okhttp3.Call;
@@ -32,8 +33,10 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private RecyclerView recyclerView;
+    private RecyclerView recyclerViewNews;
+    private RecyclerView recyclerViewFavorites;
     private CustomAdapter adapter;
+    private CustomAdapterFavorites customAdapterFavorites;
     private ListView listView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -53,20 +56,43 @@ public class HomeFragment extends Fragment {
             });
 
         // Init RecyclerView using binding
-        recyclerView = binding.recyclerViewNews;
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewNews  = binding.recyclerViewNews;
+        recyclerViewNews.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        //Init recyclerview using binding for Favorite Challenges
+        recyclerViewFavorites = binding.recyclerViewProgress;
+        recyclerViewFavorites.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // tie apiupdates list to variable
         listView = binding.apiUpdates;
 
 
 
 
         // async call to fetch the data before returning vieww
+        populateFavoritesDummy();
         fetchDataInBackground();
         updateListView();
 
 
         // must return root in order to display view.
         return root;
+    }
+
+    private void populateFavoritesDummy() {
+        List<ChallengeCard> challengeCards = new ArrayList<>();
+
+        // Create dummy challenge cards
+        challengeCards.add(new ChallengeCard(R.drawable.aek_973, "Challenge 1", "Get 20 Headshot Kills", 20, 5, true));
+        challengeCards.add(new ChallengeCard(R.drawable.ak_74, "Challenge 2", "Get 30 Kills with SMG", 30, 10, false));
+        challengeCards.add(new ChallengeCard(R.drawable.ames_85, "Challenge 3", "Use a sniper for 15 kills", 15, 3, true));
+
+
+        // Set adapter only if RecyclerView is initialized
+        if (recyclerViewFavorites != null) {
+            customAdapterFavorites = new CustomAdapterFavorites(challengeCards);
+            recyclerViewFavorites.setAdapter(customAdapterFavorites);
+        }
     }
 
     private void fetchDataInBackground() { // well have to do this because it was running on main thread.
@@ -76,9 +102,9 @@ public class HomeFragment extends Fragment {
 
             new Handler(Looper.getMainLooper()).post(() -> {
                 // Ensure recyclerView is properly set before setting the adapter
-                if (recyclerView != null) {
+                if (recyclerViewNews != null) {
                     adapter = new CustomAdapter(articles);
-                    recyclerView.setAdapter(adapter);
+                    recyclerViewNews.setAdapter(adapter);
                 }
             });
         }).start();
@@ -208,5 +234,66 @@ public class HomeFragment extends Fragment {
         }
 
 
+    }
+    public static class ChallengeCard { // use a model to define one single article the use the object properties to fill the card
+        private Integer imageId;
+        private String camoName;
+        private String camoDescription;
+        private int numRequired;
+        private int currentNumAchieved;
+        private boolean isFavourite;
+
+
+
+        public ChallengeCard(Integer imageId, String camoName, String camoDescription, int numRequired, int currentNumAchieved, boolean isFavourite) {
+            this.imageId = imageId;
+            this.camoName = camoName;
+            this.camoDescription = camoDescription;
+            this.numRequired = numRequired;
+            this.currentNumAchieved = currentNumAchieved;
+            this.isFavourite = isFavourite;
+        }
+
+    public Integer getImageId() {
+            return imageId;
+    }
+    public String getCamoName() {
+            return camoName;
+    }
+    public String getCamoDescription() {
+            return camoDescription;
+    }
+    public int getNumRequired() {
+            return numRequired;
+    }
+    public int getCurrentNumAchieved() {
+            return currentNumAchieved;
+    }
+    public boolean isFavourite() {
+            return isFavourite;
+    }
+    public void setCamoName(String camoName) {
+        this.camoName = camoName;
+    }
+
+    public void setCamoDescription(String camoDescription) {
+        this.camoDescription = camoDescription;
+    }
+
+    public void setImageId(Integer imageId) {
+        this.imageId = imageId;
+    }
+
+    public void setNumRequired(int numRequired) {
+        this.numRequired = numRequired;
+    }
+
+    public void setCurrentNumAchieved(int currentNumAchieved) {
+        this.currentNumAchieved = currentNumAchieved;
+    }
+
+    public void setFavourite(boolean isFavourite) {
+        this.isFavourite = isFavourite;
+    }
     }
 }
---
+++ app/src/main/java/com/example/gamevault/ui/home/favoritechallenges/CustomAdapterFavorites.java
@@ -0,0 +1,113 @@
+package com.example.gamevault.ui.home.favoritechallenges;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.gamevault.R;
+import com.example.gamevault.ui.home.HomeFragment.ChallengeCard;
+import java.util.List;
+
+public class CustomAdapterFavorites extends RecyclerView.Adapter<CustomAdapterFavorites.ViewHolder> {
+    private List<ChallengeCard> data;
+
+    public CustomAdapterFavorites(List<ChallengeCard> data){
+        this.data = data;
+    }
+
+    @Override
+    public CustomAdapterFavorites.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorite_challenge_layout, parent, false);
+        return new ViewHolder(rowItem);
+    }
+
+    @Override
+    public void onBindViewHolder(CustomAdapterFavorites.ViewHolder holder, int position) {
+        ChallengeCard challengeCard = data.get(position);
+        holder.imageView.setImageResource(challengeCard.getImageId());
+
+        holder.textViewTitle.setText(challengeCard.getCamoName());
+        holder.textViewDescription.setText(challengeCard.getCamoDescription());
+        holder.progressText.setText(challengeCard.getCurrentNumAchieved() + "/" + challengeCard.getNumRequired() + " Completed");
+
+
+        holder.progressBar.setMax(challengeCard.getNumRequired());
+        holder.progressBar.setProgress(challengeCard.getCurrentNumAchieved());
+
+        Glide.with(holder.imageView.getContext())
+                .load(challengeCard.getImageId())
+                .into(holder.imageView);
+
+
+        if (challengeCard.isFavourite()) {
+            holder.favoriteIcon.setColorFilter(0xFFFF0000); // Red for favorite
+        } else {
+            holder.favoriteIcon.setColorFilter(0xFFFFFFFF); // White for non-favorite
+        }
+
+        holder.subtractButton.setOnClickListener(v -> {
+            int newProgress = Math.max(0, challengeCard.getCurrentNumAchieved() - 1);
+            challengeCard.setCurrentNumAchieved(newProgress);
+            holder.progressText.setText(newProgress + "/" + challengeCard.getNumRequired() + " Completed");
+            holder.progressBar.setProgress(newProgress);
+        });
+
+        holder.addButton.setOnClickListener(v -> {
+            int newProgress = Math.min(challengeCard.getNumRequired(), challengeCard.getCurrentNumAchieved() + 1);
+            challengeCard.setCurrentNumAchieved(newProgress);
+            holder.progressText.setText(newProgress + "/" + challengeCard.getNumRequired() + " Completed");
+            holder.progressBar.setProgress(newProgress);
+        });
+
+        // Handle favorite icon toggle
+        holder.favoriteIcon.setOnClickListener(v -> {
+            challengeCard.setFavourite(!challengeCard.isFavourite());
+            if (challengeCard.isFavourite()) {
+                holder.favoriteIcon.setColorFilter(0xFFFF0000);
+            } else {
+                holder.favoriteIcon.setColorFilter(0xFFFFFFFF);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private ImageView imageView;
+        private ImageView subtractButton;
+        private ImageView addButton;
+        private ProgressBar progressBar;
+        private ImageView favoriteIcon;
+        private TextView progressText;
+        private ImageView checkMarkIcon;
+
+        public ViewHolder(View view) {
+            super(view);
+            view.setOnClickListener(this);
+            this.textViewTitle = view.findViewById(R.id.title);
+            this.textViewDescription = view.findViewById(R.id.description);
+            this.imageView = view.findViewById(R.id.weaponImage);
+            this.subtractButton = view.findViewById(R.id.subtract);
+            this.addButton = view.findViewById(R.id.add);
+            this.progressBar = view.findViewById(R.id.progressBar);
+            this.favoriteIcon = view.findViewById(R.id.favorite);
+            this.progressText = view.findViewById(R.id.progressText);
+            this.checkMarkIcon = view.findViewById(R.id.imageView6);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), "Tapped on challenge", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
