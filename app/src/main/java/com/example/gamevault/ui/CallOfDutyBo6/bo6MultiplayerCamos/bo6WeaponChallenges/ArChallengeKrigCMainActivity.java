63c28101d308cb278e7bb087c36e5e7e016bc74e
xSpeedyMonkeyx <clwallace1@student.fullsail.edu>
2025-02-20 14:08:23 -0500

    AR Layout Additions w/ Click Listeners (#64)
    * Added Remaining AR's challenge screens in BO6 Multiplayer
    
    * updated bo6ARActivity for all AR click listeners

---
+++ app/src/main/java/com/example/gamevault/ui/CallOfDutyBo6/bo6MultiplayerCamos/bo6WeaponChallenges/ArChallengeKrigCMainActivity.java
@@ -0,0 +1,22 @@
+package com.example.gamevault.ui.CallOfDutyBo6.bo6MultiplayerCamos.bo6WeaponChallenges;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.gamevault.R;
+
+public class ArChallengeKrigCMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar_challenge_krig_c_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
